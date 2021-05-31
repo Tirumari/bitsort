92aa503b76fbee7c6acc56e845b80a93d4717cd0
Tirumari <tirumari@gmail.com>
2021-05-30 17:41:32 -0700

    connects to atlas mongodb

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,22 +1,15 @@
-const mongoose = require('mongoose');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient
 const config = require('config');
 const db = config.get('mongoURI');
 
-// db connection method
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      db,
-      {
-        useNewUrlParser: true
-      }
-    );
-
-    console.log('MongoDB is Connected...');
+    await MongoClient.connect(db, { useNewUrlParser: true, useUnifiedTopology: true } )
+    console.log('MongoDB connected')
   } catch (err) {
-    console.error(err.message);
-    process.exit(1);
+    console.log(err.message)
   }
-};
+}
 
 module.exports = connectDB;
